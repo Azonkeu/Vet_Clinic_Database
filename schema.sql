83be7405d94390a9751be66749030f2b861c6d38
Azonkeu <mazonkeu@gmail.com>
2022-03-26 22:13:28 +0100

    Add join tables

--- schema.sql
+++ schema.sql
@@ -14,9 +14,24 @@ CREATE TABLE animals(
       REFERENCES species(id),
 	CONSTRAINT fk_owners
     FOREIGN KEY (owner_id) 
-      REFERENCES owners(id)
+      REFERENCES owners(id),
+		UNIQUE(id)
 );
 
+CREATE TABLE visits(
+	  visit_id           INT NOT NULL,
+		names_id              INT NOT NULL,
+		date_of_visits       DATE NOT NULL,
+		CONSTRAINT fk_animals
+      FOREIGN KEY (visit_id) 
+        REFERENCES animals(id)
+			   ON DELETE RESTRICT ON UPDATE CASCADE,
+	  CONSTRAINT fk_vets
+      FOREIGN KEY (names_id ) 
+        REFERENCES vets(id)
+				ON DELETE RESTRICT ON UPDATE CASCADE,
+		PRIMARY KEY(visit_id, names_id)
+);
 CREATE TABLE owners(
 	  id           INT GENERATED ALWAYS AS IDENTITY NOT NULL,
 		full_name    VARCHAR(255) NOT NULL,
@@ -30,3 +45,29 @@ CREATE TABLE species(
 		PRIMARY KEY(id)
 );
 
+
+CREATE TABLE vets(
+	  id                   INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+		name                 VARCHAR(255) NOT NULL,
+		age                  INT NOT NULL,
+		date_of_graduation   DATE NOT NULL,
+		PRIMARY KEY(id)
+);
+
+
+CREATE TABLE specializations (
+  id SERIAL PRIMARY KEY,
+  species_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  FOREIGN KEY (species_id) REFERENCES species (id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
+
+CREATE TABLE visits (
+  visit_id SERIAL PRIMARY KEY,
+  animal_id INT NOT NULL,
+  vet_id INT NOT NULL,
+  date_of_visit DATE,
+  FOREIGN KEY (animal_id) REFERENCES animals (id),
+  FOREIGN KEY (vet_id) REFERENCES vets (id)
+);
