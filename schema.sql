6d906c68ced20dd4877339f75d61f6aee4da6db3
Azonkeu <mazonkeu@gmail.com>
2022-03-30 12:38:43 +0100

    Update queries

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-	id                INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+	id                INT GENERATED ALWAYS AS IDENTITY NOT NULL,
 	name              VARCHAR  NOT NULL,
 	date_of_birth     DATE NOT NULL,
 	escape_attempts   INT NOT NULL,
@@ -15,23 +15,9 @@ CREATE TABLE animals(
 	CONSTRAINT fk_owners
     FOREIGN KEY (owner_id) 
       REFERENCES owners(id),
-		UNIQUE(id)
+	PRIMARY KEY(id)
 );
 
-CREATE TABLE visits(
-	  visit_id           INT NOT NULL,
-		names_id              INT NOT NULL,
-		date_of_visits       DATE NOT NULL,
-		CONSTRAINT fk_animals
-      FOREIGN KEY (visit_id) 
-        REFERENCES animals(id)
-			   ON DELETE RESTRICT ON UPDATE CASCADE,
-	  CONSTRAINT fk_vets
-      FOREIGN KEY (names_id ) 
-        REFERENCES vets(id)
-				ON DELETE RESTRICT ON UPDATE CASCADE,
-		PRIMARY KEY(visit_id, names_id)
-);
 CREATE TABLE owners(
 	  id           INT GENERATED ALWAYS AS IDENTITY NOT NULL,
 		full_name    VARCHAR(255) NOT NULL,
@@ -45,7 +31,6 @@ CREATE TABLE species(
 		PRIMARY KEY(id)
 );
 
-
 CREATE TABLE vets(
 	  id                   INT GENERATED ALWAYS AS IDENTITY NOT NULL,
 		name                 VARCHAR(255) NOT NULL,
@@ -55,19 +40,30 @@ CREATE TABLE vets(
 );
 
 
-CREATE TABLE specializations (
-  id SERIAL PRIMARY KEY,
-  species_id INT NOT NULL,
-  vet_id INT NOT NULL,
-  FOREIGN KEY (species_id) REFERENCES species (id),
-  FOREIGN KEY (vet_id) REFERENCES vets (id)
+CREATE TABLE specializations(
+    id                   INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    specie_id            INT NOT NULL,
+    vet_id               INT NOT NULL,
+    CONSTRAINT fk_specie
+		  FOREIGN KEY (specie_id )
+        REFERENCES species(id),
+	  CONSTRAINT fk_vet
+       FOREIGN KEY (vet_id)
+        REFERENCES vets(id),
+	  PRIMARY KEY (id)
 );
 
-CREATE TABLE visits (
-  visit_id SERIAL PRIMARY KEY,
-  animal_id INT NOT NULL,
-  vet_id INT NOT NULL,
-  date_of_visit DATE,
-  FOREIGN KEY (animal_id) REFERENCES animals (id),
-  FOREIGN KEY (vet_id) REFERENCES vets (id)
+
+CREATE TABLE visits(
+    id                   INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    animal_id            INT NOT NULL,
+    vete_id              INT NOT NULL,
+    date_of_visit        DATE,
+    CONSTRAINT fk_animal
+      FOREIGN KEY (animal_id)
+        REFERENCES animals(id),
+	  CONSTRAINT fk_vete
+      FOREIGN KEY (vete_id)
+        REFERENCES vets(id),
+	  PRIMARY KEY (id)
 );
